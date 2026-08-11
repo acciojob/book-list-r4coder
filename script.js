dab324c67dfd0e6d8f03eeeee6e8774d7e32fb54
r4coder <ritheeshk2003@gmail.com>
2026-08-11 09:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+
+const submit = document.getElementById("submit");
+const bookList = document.getElementById("book-list");
+
+// Add book
+submit.addEventListener("click", function () {
+    const titleValue = title.value;
+    const authorValue = author.value;
+    const isbnValue = isbn.value;
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${titleValue}</td>
+        <td>${authorValue}</td>
+        <td>${isbnValue}</td>
+        <td>
+            <button class="delete">Clear</button>
+        </td>
+    `;
+
+    bookList.appendChild(row);
+
+    title.value = "";
+    author.value = "";
+    isbn.value = "";
+});
+
+// Delete book
+bookList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) {
+        const row = event.target.parentElement.parentElement;
+        row.remove();
+    }
+});
